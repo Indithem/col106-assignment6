fe47f1f193940f7566119fe17b756ffb3e9bf3e7
Sai Srinivas <128889127+Indithem@users.noreply.github.com>
2023-10-23 19:14:56 +0530

    updates word_dist

--- word_dist.py
+++ word_dist.py
@@ -16,8 +16,22 @@ for word in english_words:
     # first_letter = word[0]
     word_length = len(word)
     # hash_value = mmh3.hash(word)
-    # key = f"{first_letter}{word_length}"
-    word_distribution[word_length] += 1
+    if 3<=word_length<=16:
+        # first_letter=1 if word[0]<'m' else 2
+        # if 5<=word_length<=14:
+        # first_letter= 1 if word[0]<'f' else 2 if word[0]<'m' else 3 if word[0]<'u' else 4
+        first_letter=mmh3.hash(word)%10
+        # first_letter=''
+
+    else:
+        first_letter=''
+    key = f"{word_length}_{first_letter}"
+    word_distribution[key] += 1
+
+# Following Brevity law see: https://en.wikipedia.org/wiki/Brevity_law
+# for item in word_distribution.items():
+    # word_distribution[item[0]]=item[1]/int(item[0].split('_')[0])
+#our distribution must have constant product of le
 
 sorted_data = dict(sorted(word_distribution.items(), key=lambda item: item[1], reverse=True))
 
