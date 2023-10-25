989692671f6f1c1d62c5425e14758b82dd1f8db9
Hanumath2005 <cs1221608@iitd.ac.in>
2023-10-25 23:18:39 +0530

    part-a

--- dict.h
+++ dict.h
@@ -4,70 +4,32 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-
-constexpr unsigned WordsDict_BUCKET = 4096;
-constexpr unsigned seed = 250053465;
-constexpr unsigned bucket_size[] ={
-    0,//0
-    0,0,// 1 and 2  //if any has 0, then it means that size will get merged with next non-zero size
-    0,  //3
-    2,  //4  
-    2,  //5
-    3,  //6
-    3,  //7
-    3,  //8
-    3,  //9
-    3,  //10
-    2,  //11
-    2,  //12
-    1,  //13
-    1,  //14
-    1  //higher
+class Hash_table{
+    public:
+        vector<Trie_Node*>children_hash_table;
+        unsigned int size_of_children_hash_table=256;
+        Hash_table();
+        ~Hash_table();
 };
-
-int unsigned murmurhash(const std::string&);
-
-//nodes for linked list in WordsDict
-struct Node{
-    Node* next=nullptr;
-    std::string* name=nullptr;
-    unsigned count=0;
-    Node() = delete;
-    Node(std::string&);
-    ~Node();
+class Trie_Node{
+    bool Word_Ending;
+    unsigned int count;
+    Hash_table*children;
+    public:
+        Trie_Node();
+        ~Trie_Node();
 };
-
-//a normal hashtable
-class WordsDict{
-public:
-    vector<Node*> data;
-
-public:
-    WordsDict();
-    void insert(std::string&,unsigned h);
-    void increment(std::string&,unsigned h);
-    void dump(std::ofstream&);
-    unsigned get_count(std::string&,unsigned h);
-    ~WordsDict();
-};
-
-//this will contain instances of WordsDict
-class MainDict{
-private:
-    vector<WordsDict*> data;
-
-public:
-    MainDict();
-    void insert(std::string&);
-    void increment(std::string&);
-    void dump(std::string&);
-    unsigned get_count(std::string&);
-    ~MainDict();
+class Trie_of_dictionary{
+    Trie_Node*root;
+    public:
+        Trie_of_dictionary();
+        ~Trie_of_dictionary();
+        void insert(string word_in_sentence);
+        int search(string word);
 };
 
 class Dict {
 private:
-    MainDict dictionary;
     const std::string seperator =  " .,-:!\"\'()?—[]“”‘’˙;@"; 
     // You can add attributes/helper functions here
 
