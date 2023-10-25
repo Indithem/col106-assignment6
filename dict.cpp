c8146520888651cca515250ddbc914f052494ecd
Indithem <128889127+Indithem@users.noreply.github.com>
2023-10-26 00:15:48 +0530

    done

--- dict.cpp
+++ dict.cpp
@@ -23,7 +23,7 @@ Trie_of_dictionary::Trie_of_dictionary(){
 Trie_of_dictionary::~Trie_of_dictionary(){
     delete root;
 }
-void Trie_of_dictionary::insert(string word_in_sentence){
+void Trie_of_dictionary::insert(const string& word_in_sentence){
     Trie_Node*temp_root=root;
     unsigned int index;
     Trie_Node*insertor;
@@ -47,7 +47,7 @@ void Trie_of_dictionary::insert(string word_in_sentence){
         temp_root->children->children_hash_table[index]->count+=1;
     }
 }
-unsigned int Trie_of_dictionary::search(string word){
+unsigned int Trie_of_dictionary::search(const string& word){
     Trie_Node*temp_root=root;
     unsigned int index;
     for(int i=0;i<word.size();i++){
@@ -58,4 +58,92 @@ unsigned int Trie_of_dictionary::search(string word){
         temp_root=temp_root->children->children_hash_table[index];
     }
     return temp_root->count;
+}Dict::Dict(){}
+Dict::~Dict(){}
+
+void Dict::insert_sentence(int& book_code, int& page, int& paragraph, int& sentence_no, const string& sentence){
+    string word;
+    size_t start=0;
+    size_t end;
+    size_t length=sentence.length();
+
+     while (start < length) {
+        /*invariants
+            we dont know if we need to include whatever is present at start
+        */
+        bool cont=true;
+        for(end=start; end<length and cont; end++ ){
+            /*invariants
+                idk if [end] is a seperator
+            */
+            char letter = sentence[end];
+            for(const char&c :seperator)
+                {if(c==letter)
+                {cont=false;break;}}
+        }
+        end+=cont;
+        /*
+            [end-1] is a seperator or the end of word but not both,
+            end-1 could be start
+
+            [][][][][][][][start]...end
+        */
+
+        word = sentence.substr(start, end - start-1);
+        start=end;
+
+        if(word.empty()){continue;}
+
+        for(char&c :word){
+            c=tolower(c);
+        }       
+
+        dict.insert(word);
+
+    }
+
+    return;
+}
+
+unsigned Dict::get_word_count(const string& word){
+    return dict.search(word);
+}
+
+void Dict::dump_dictionary(string& fp){
+    ofstream out;
+    out.open(fp,ios_base::out);
+
+    string word;
+    vector<Trie_Node*> stack;
+
+    Trie_Node* current;
+    Trie_Node* next;
+    stack.push_back(dict.root);
+    //assume we visited/printed all elements in stack
+
+    char i=0; //currently visiting i
+
+    while(!stack.empty())
+    {
+    current=stack.back();
+    while(i<Hash_table::size_of_children_hash_table){
+        next=current->children->children_hash_table[i];
+        if(next==nullptr){i++;continue;}
+
+        word.push_back(i);
+        if(next->Word_Ending){
+            out<<word<<", "<<next->count<<'\n';
+        }
+        stack.push_back(next);
+        current=next;
+        i=0;
+    }
+
+    stack.pop_back();
+    if(word.empty()){return;}
+    i = word.back()+1;
+    word.pop_back();
+
+    }
+
 }
--- dict.h
+++ dict.h
@@ -4,18 +4,18 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+struct Trie_Node;
 class Hash_table{
     public:
         vector<Trie_Node*>children_hash_table;
-        unsigned int size_of_children_hash_table=256;
+        constexpr static unsigned int size_of_children_hash_table=256;
         Hash_table();
         ~Hash_table();
 };
-class Trie_Node{
-    public:
-        bool Word_Ending;
-        unsigned int count;
-        Hash_table*children;
+struct Trie_Node{
+    bool Word_Ending;
+    unsigned int count;
+    Hash_table*children;
         Trie_Node();
         ~Trie_Node();
 };
@@ -24,12 +24,13 @@ class Trie_of_dictionary{
         Trie_Node*root;
         Trie_of_dictionary();
         ~Trie_of_dictionary();
-        void insert(string word_in_sentence);
-        unsigned int search(string word);
+        void insert(const string& word_in_sentence);
+        unsigned int search(const string& word);
 };
 
 class Dict {
 private:
+    Trie_of_dictionary dict;
     const std::string seperator =  " .,-:!\"\'()?—[]“”‘’˙;@"; 
     // You can add attributes/helper functions here
 
@@ -43,9 +44,9 @@ public:
 
     void insert_sentence(int& book_code, int& page, int& paragraph, int& sentence_no, const string& sentence);
 
-    unsigned get_word_count(string word);
+    unsigned get_word_count(const string& word);
 
-    void dump_dictionary(string filename);
+    void dump_dictionary(string& filename);
 
     /* -----------------------------------------*/
 };
