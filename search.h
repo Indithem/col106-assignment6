014d386759f3e00b99d737a894f3fd16a92c4615
Indithem <128889127+Indithem@users.noreply.github.com>
2023-10-25 21:09:13 +0530

    Declrations of objects to be used

--- search.h
+++ search.h
@@ -5,9 +5,38 @@
 #include "Node.h"
 using namespace std;
 
+constexpr unsigned seed = 250053465;
+int unsigned murmurhash(const std::string&);
+
+struct LinkedNode{
+    LinkedNode* next=nullptr;
+    int offsetof;
+    LinkedNode()=delete;
+    LinkedNode(size_t);
+    ~LinkedNode();
+};
+
+class SentenceHashTable{
+private:
+    constexpr static unsigned HASHTABLE_SIZE=256;
+
+    vector<LinkedNode*> table;
+    int book_code=0;
+    int page=0;
+    int paragraph=0;
+    int sentence_no=0;
+public:
+    string sentence;
+    SentenceHashTable() = delete;
+    SentenceHashTable(string& sentence,int book_cod, int pag, int paragrap, int sentence_n);
+    LinkedNode* get_node(char);
+    ~SentenceHashTable();
+};
+
 class SearchEngine {
 private:
     // You can add attributes/helper functions here
+    vector<SentenceHashTable*> sentences;
 
 public: 
     /* Please do not touch the attributes and 
